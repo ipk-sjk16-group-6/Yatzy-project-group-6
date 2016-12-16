3fa883480385733bd37345e10e40b2ee0cece226
MaxieTheHatter <maxnelson90@gmail.com>
2016-12-16 19:52:52 +0100

    Fixed bug with bonus

--- Yatzy/Model.java
+++ Yatzy/Model.java
@@ -257,6 +257,7 @@ public class Model {
         currentPlayer.currentRoll++;
         System.out.println("Current roll: " + currentPlayer.currentRoll);
         if (currentPlayer.currentRoll == 1) {
+            roll();
             resetScoreButtons();
             emptySavedValuesDiceCast();
             emptyMarkedDices();
